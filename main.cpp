1db1b7bce0d02e88ab0ed01e93fe3aa4aeddca89
Darkdarksun <4221spammer@gmail.com>
2021-07-17 17:40:48 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile file (":/stylesheets/style.css");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(file.readAll());
     MainWindow w;
     w.show();
     return a.exec();
 }
-
