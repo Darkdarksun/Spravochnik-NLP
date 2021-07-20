3552edb1049fc4319b73b1dcc8b54451d27a207f
Darkdarksun <4221spammer@gmail.com>
2021-07-20 18:02:51 +0300

    window resize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,43 +27,50 @@ void MainWindow::on_pB1_clicked()
 
 void MainWindow::on_pB2_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,2205, 20, 1);
-    ui->scrollArea->ensureVisible(0,2205+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,2205+winsize, 20, 1);
 }
 
 void MainWindow::on_pB3_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,3195, 20, 1);
-    ui->scrollArea->ensureVisible(0,3195+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,3195+winsize, 20, 1);
 }
 
 void MainWindow::on_pB4_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,6270, 20, 1);
-    ui->scrollArea->ensureVisible(0,6270+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,6270+winsize, 20, 1);
 }
 
 void MainWindow::on_pB5_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,7950, 20, 1);
-    ui->scrollArea->ensureVisible(0,7950+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,7950+winsize, 20, 1);
 }
 
 void MainWindow::on_pB6_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,9389, 20, 1);
-    ui->scrollArea->ensureVisible(0,9389+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,9389+winsize, 20, 1);
 }
 
 void MainWindow::on_pB7_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,10409, 20, 1);
-    ui->scrollArea->ensureVisible(0,10409+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,10409+winsize, 20, 1);
 }
 
 void MainWindow::on_pB8_clicked()
 {
+    int winsize=ui->scrollArea->height();
     ui->scrollArea->ensureVisible(0,12987, 20, 1);
-    ui->scrollArea->ensureVisible(0,12987+950, 20, 1);
+    ui->scrollArea->ensureVisible(0,12987+winsize, 20, 1);
 }
 
